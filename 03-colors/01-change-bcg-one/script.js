3c2af202da29e66e58e8235aa37dfb00b0ace45e
Christophe Adriaensens <christophe@ChristophesiMac.lan>
2020-11-10 15:18:43 +0100

    Finish exercises 03-colors

---
+++ 03-colors/01-change-bcg-one/script.js
@@ -0,0 +1,31 @@
+/* becode/javascript
+ *
+ * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.getElementById("red").addEventListener("click", function(){
+        var color = '#ff0000';
+        document.body.style.backgroundColor = color;
+    })
+    document.getElementById("green").addEventListener("click", function(){
+        var color = '#00ff40';
+        document.body.style.backgroundColor = color;
+    })
+    document.getElementById("yellow").addEventListener("click", function(){
+        var color = '#ffff00';
+        document.body.style.backgroundColor = color;
+    })
+    document.getElementById("blue").addEventListener("click", function(){
+        var color = '#0080ff';
+        document.body.style.backgroundColor = color;
+    })
+})();
---
+++ 03-colors/03-change-bcg-three/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /03-colors/03-change-bcg-three/script.js - 3.3: couleur de fond (3)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        console.log(r,g,b);
+        var color = 'rgb'+'('+r+','+g+','+b+')';
+        document.body.style.backgroundColor = color;
+    })
+
+
+
+})();
